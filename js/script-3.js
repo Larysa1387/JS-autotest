1c6ea52331e925b94788e78ba11157ef0531478a
Larysa1387 <kaliuzhna.off@gmail.com>
2021-02-27 00:52:34 +0200

    25 tasks done

--- js/script-3.js
+++ js/script-3.js
@@ -327,14 +327,16 @@
 // .............................................................................
 // Module 3 Test 14
 // .............................................................................
-// const apartment = {
-//   descr: 'Просторная квартира в центре',
-//   rating: 4,
-//   price: 2153,
-// };
-// // Пиши код ниже этой строки
-// const keys = Object.keys(apartment);
-// const values = Object.values(apartment);
+// function countProps(object) {
+//   // Пиши код ниже этой строки
+//   let propCount = 0;
+
+//  propCount = Object.keys(object).length;
+
+//   return propCount;
+//   // Пиши код выше этой строки
+// }
+
 
 
 // .............................................................................
@@ -353,3 +355,249 @@
 // .............................................................................
 // Module 3 Test 16
 // .............................................................................
+// function countTotalSalary(salaries) {
+//   let totalSalary = 0;
+
+// Запасной вариант:
+//   const values = [];
+//   for (const key in salaries) {
+//     values.push(salaries[key]);
+//     totalSalary += salaries[key];
+// }
+
+// Верный вариант для бота
+//   const values = [];
+//   const keys = Object.keys(salaries);
+
+//   for (const key of keys) {
+//   values.push(salaries[key]);
+//   totalSalary += salaries[key];
+// }
+//   // Пиши код выше этой строки
+//   return totalSalary;
+// }
+// console.log(countTotalSalary({ mango: 100, poly: 150, alfred: 80 })); // 330
+
+
+// .............................................................................
+// Module 3 Test 17
+// .............................................................................
+// const colors = [
+//   { hex: '#f44336', rgb: '244,67,54' },
+//   { hex: '#2196f3', rgb: '33,150,243' },
+//   { hex: '#4caf50', rgb: '76,175,80' },
+//   { hex: '#ffeb3b', rgb: '255,235,59' },
+// ];
+
+// const hexColors = [];
+// const rgbColors = [];
+// // Пиши код ниже этой строки
+// for (const color of colors) {
+// hexColors.push(color.hex);
+// rgbColors.push(color.rgb);
+// }
+// console.log(hexColors); // [ '#f44336', '#2196f3', '#4caf50', '#ffeb3b' ]
+// console.log(rgbColors); // [ '244,67,54', '33,150,243', '76,175,80', '255,235,59' ]
+
+
+// .............................................................................
+// Module 3 Test 18
+// .............................................................................
+// const products = [
+//   { name: 'Радар', price: 1300, quantity: 4 },
+//   { name: 'Сканер', price: 2700, quantity: 3 },
+//   { name: 'Дроид', price: 400, quantity: 7 },
+//   { name: 'Захват', price: 1200, quantity: 9 },
+// ];
+
+// function getProductPrice(productName) {
+//   // Пиши код ниже этой строки
+//   for (let product of products) {
+//     if (product.name === productName) {
+//       return product.price;
+//     }
+//   }
+//   return null;
+
+//   // Пиши код выше этой строки
+// }
+// console.log(getProductPrice('Радар'));  // 1300
+// console.log(getProductPrice('Кука'));  // null
+
+// .............................................................................
+// Module 3 Test 19
+// .............................................................................
+// const products = [
+//   { name: 'Радар', price: 1300, quantity: 4 },
+//   { name: 'Сканер', price: 2700, quantity: 3 },
+//   { name: 'Дроид', price: 400, quantity: 7 },
+//   { name: 'Захват', price: 1200, quantity: 9 },
+// ];
+
+// function getAllPropValues(propName) {
+//   // Пиши код ниже этой строки
+//   let values = [];
+
+//   for (let product of products) {
+//     const arrayOfNames = Object.keys(product);
+//     // console.log(arrayOfNames);
+
+//     for (let key of arrayOfNames) {
+//       if (key === propName) {
+//         // console.log(value);
+//         values.push(product[key]);
+//       }
+//     }
+//   }
+//   return values;
+// }
+
+// console.log(getAllPropValues('name'));  // ['Радар', 'Сканер', 'Дроид', 'Захват']
+// console.log(getAllPropValues('quantity')); // [4, 3, 7, 9]
+
+
+// .............................................................................
+// Module 3 Test 20
+// .............................................................................
+// const products = [
+//   { name: 'Радар', price: 1300, quantity: 4 },
+//   { name: 'Сканер', price: 2700, quantity: 3 },
+//   { name: 'Дроид', price: 400, quantity: 7 },
+//   { name: 'Захват', price: 1200, quantity: 9 },
+// ];
+
+// function calculateTotalPrice(productName) {
+//   // Пиши код ниже этой строки
+//   let total = 0;
+
+//   for (const product of products) {
+//     // let name = product.name;
+//     if (productName === product.name) {
+//       total += product.price * product.quantity;
+//     }
+//   }
+//     return total;
+//   // Пиши код выше этой строки
+// }
+// console.log(calculateTotalPrice('Радар'));
+// console.log(calculateTotalPrice('Дроид'));
+// console.log(calculateTotalPrice('Бластер'));
+
+
+// .............................................................................
+// Module 3 Test 21
+// .............................................................................
+// const highTemperatures = {
+//   yesterday: 28,
+//   today: 26,
+//   tomorrow: 33,
+// };
+// // Пиши код ниже этой строки
+
+// const {yesterday, today, tomorrow} = highTemperatures;
+// // Пиши код выше этой строки
+// const meanTemperature = (yesterday + today + tomorrow) / 3;
+
+// console.log(meanTemperature);
+
+
+// .............................................................................
+// Module 3 Test 22
+// .............................................................................
+// const highTemperatures = {
+//   yesterday: 28,
+//   today: 26,
+//   tomorrow: 33,
+// };
+// // Пиши код ниже этой строки
+
+// const {
+//   yesterday,
+//   today,
+//   tomorrow,
+//   icon = 'https://www.flaticon.com/svg/static/icons/svg/2204/2204346.svg',
+// } = highTemperatures;
+
+// // Пиши код выше этой строки
+// const meanTemperature = (yesterday + today + tomorrow) / 3;
+// console.log(meanTemperature);
+
+// .............................................................................
+// Module 3 Test 23
+// .............................................................................
+const highTemperatures = {
+  yesterday: 28,
+  today: 26,
+  tomorrow: 33,
+};
+// Пиши код ниже этой строки
+const {
+  yesterday: highYesterday,
+  today: highToday,
+  tomorrow: highTomorrow,
+  icon: highIcon = 'https://www.flaticon.com/svg/static/icons/svg/2204/2204346.svg',
+} = highTemperatures;
+// Пиши код выше этой строки
+const meanTemperature = (highYesterday + highToday + highTomorrow) / 3;
+console.log(meanTemperature);
+
+// .............................................................................
+// Module 3 Test 24
+// .............................................................................
+// ПРИМЕР
+// const books = [
+//   {
+//     title: 'Последнее королевство',
+//     author: 'Бернард Корнуэлл',
+//     rating: 8.38,
+//   },
+//   {
+//     title: 'На берегу спокойных вод',
+//     author: 'Роберт Шекли',
+//     rating: 8.51,
+//   },
+// ];
+
+// for (const book of books) {                                 for (const { title, author, rating } of books) {
+//   const { title, author, rating } = book;                     console.log(title);
+//                                                               console.log(author);
+//   console.log(title);                                         console.log(rating);
+//   console.log(author);                                        }
+//   console.log(rating);
+// }
+// .................................................................................
+const colors = [
+  { hex: '#f44336', rgb: '244,67,54' },
+  { hex: '#2196f3', rgb: '33,150,243' },
+  { hex: '#4caf50', rgb: '76,175,80' },
+  { hex: '#ffeb3b', rgb: '255,235,59' },
+];
+
+const hexColors = [];
+const rgbColors = [];
+// Пиши код ниже этой строки
+
+for (const {hex, rgb} of colors) {
+  hexColors.push(hex);
+  rgbColors.push(rgb);
+}
+
+
+// .............................................................................
+// Module 3 Test 25
+// .............................................................................
+const forecast = {
+  today: {
+    low: 28,
+    high: 32,
+    icon: 'https://www.flaticon.com/svg/static/icons/svg/861/861059.svg',
+  },
+  tomorrow: {
+    low: 27,
+    high: 31,
+  },
+};
+// Пиши код ниже этой строки
+
+const { today: { high: highToday, low: lowToday, icon: todayIcon = 'https://www.flaticon.com/svg/static/icons/svg/2204/2204346.svg' },
+  tomorrow: { high: highTomorrow, low: lowTomorrow, icon: tomorrowIcon = 'https://www.flaticon.com/svg/static/icons/svg/2204/2204346.svg' } } = forecast;
