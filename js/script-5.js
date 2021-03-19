4f219b56f15a38b481d17aeeec8562b1a5985205
Larysa1387 <kaliuzhna.off@gmail.com>
2021-03-19 11:40:07 +0200

    finished module 5

--- js/script-5.js
+++ js/script-5.js
@@ -461,24 +461,152 @@
 //   User.isEmailTaken('mango@mail.com')
 // ); // true
 
+// ......Test 16.......................................................................
+// class Car {
+//   static #MAX_PRICE = 50000;
+//   // Пиши код ниже этой строки
+//   static checkPrice(price) {
+//   	if (price > Car.#MAX_PRICE) {
+// 		return 'Внимание! Цена превышает допустимую.';
+//   	}
+//     return 'Всё хорошо, цена в порядке.';
+//   }
+//   // Пиши код выше этой строки
+//   constructor({ price }) {
+//     this.price = price;
+//   }
+// }
+
+// const audi = new Car({ price: 36000 });
+// const bmw = new Car({ price: 64000 });
+
+// console.log(Car.checkPrice(audi.price)); // Всё хорошо, цена в порядке.
+// console.log(Car.checkPrice(bmw.price)); // Внимание! Цена превышает допустимую.
+
 // ..............................................................................
-class Car {
-  static #MAX_PRICE = 50000;
-  // Пиши код ниже этой строки
-  static checkPrice(price) {
-  	if (price > Car.#MAX_PRICE) {
-		return 'Внимание! Цена превышает допустимую.';
-  	}
-    return 'Всё хорошо, цена в порядке.';
+// Module 5 test 17
+// ...............................................................................
+// class User {
+//   email;
+
+//   constructor(email) {
+//     this.email = email;
+//   }
+
+//   get email() {
+//     return this.email;
+//   }
+
+//   set email(newEmail) {
+//     this.email = newEmail;
+//   }
+// }
+//   // Пиши код ниже этой строки
+//   class Admin extends User {
+//   static AccessLevel = {
+//       BASIC: 'basic',
+//       SUPERUSER: 'superuser',
+//     };
+//   }
+// console.log(Admin.AccessLevel.BASIC); //  basic
+
+
+// ..............................................................................
+// Module 5 test 18
+// ...............................................................................
+// class User {
+//   email;
+
+//   constructor(email) {
+//     this.email = email;
+//   }
+
+//   get email() {
+//     return this.email;
+//   }
+
+//   set email(newEmail) {
+//     this.email = newEmail;
+//   }
+// }
+
+// class Admin extends User {
+//   // Пиши код ниже этой строки
+// 	accessLevel;
+//   static AccessLevel = {
+//     BASIC: 'basic',
+//     SUPERUSER: 'superuser'
+//   };
+
+//   constructor({email, accessLevel}) {
+//   super(email);
+//   this.accessLevel = accessLevel;
+//   }
+//   // Пиши код выше этой строки
+// }
+
+// const mango = new Admin({
+//   email: 'mango@mail.com',
+//   accessLevel: Admin.AccessLevel.SUPERUSER
+// });
+
+// console.log(mango.email); // mango@mail.com
+// console.log(mango.accessLevel); // superuser
+
+
+// ..............................................................................
+// Module 5 test 19
+// ...............................................................................
+class User {
+  email;
+
+  constructor(email) {
+    this.email = email;
   }
-  // Пиши код выше этой строки
-  constructor({ price }) {
-    this.price = price;
+
+  get email() {
+    return this.email;
+  }
+
+  set email(newEmail) {
+    this.email = newEmail;
   }
 }
+class Admin extends User {
+  // Пиши код ниже этой строки
 
-const audi = new Car({ price: 36000 });
-const bmw = new Car({ price: 64000 });
+  static AccessLevel = {
+    BASIC: 'basic',
+    SUPERUSER: 'superuser'
+  };
 
-console.log(Car.checkPrice(audi.price)); // Всё хорошо, цена в порядке.
-console.log(Car.checkPrice(bmw.price)); // Внимание! Цена превышает допустимую.
+  accessLevel;
+  blacklistedEmails = [];
+
+
+  constructor({ email, accessLevel }) {
+    super(email);
+    this.accessLevel = accessLevel;
+  }
+
+  blacklist(email){
+  this.blacklistedEmails.push(email);
+  }
+
+  isBlacklisted(email){
+    return this.blacklistedEmails.includes(email);
+  }
+  // Пиши код выше этой строки
+}
+
+const mango = new Admin({
+  email: 'mango@mail.com',
+  accessLevel: Admin.AccessLevel.SUPERUSER
+});
+
+console.log(mango.email); // mango@mail.com
+console.log(mango.accessLevel); // superuser
+mango.blacklist('poly@mail.com');
+console.log(mango.blacklistedEmails); // 'poly@mail.com'
+console.log(mango.isBlacklisted('mango@mail.com')); //  false
+console.log(mango.isBlacklisted('poly@mail.com')); // true
